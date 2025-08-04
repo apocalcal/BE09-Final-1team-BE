4f5a26b5b56efb60a9fb6d664718bf1209ee709f
heechae <chaehee15@naver.com>
2025-08-04 12:19:08 +0900

    feat: news-service config 포함 초기 프로젝트 생성

--- services/news-service/src/main/java/com/newnormallist/newsservice/NewsServiceApplication.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/NewsServiceApplication.java
@@ -1,15 +1,13 @@
-package com.newsservice.news;
+package com.newnormallist.newsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableScheduling
 public class NewsServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(NewsServiceApplication.class, args);
     }
+
 }
