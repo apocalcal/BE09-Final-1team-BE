1d2df07512b021ae6ce41b26ab5b054f0e4dd1d1
parkchangjun <parkchangjun13@gmail.com>
2025-08-24 20:19:45 +0900

    [Fix] : 스크랩 관련 쿼리 및 메서드 수정

--- services/news-service/src/main/java/com/newsservice/news/repository/NewsScrapRepository.java
+++ services/news-service/src/main/java/com/newsservice/news/repository/NewsScrapRepository.java
@@ -1,5 +1,6 @@
 package com.newsservice.news.repository;
 
+import com.newsservice.news.entity.Category;
 import com.newsservice.news.entity.NewsScrap;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -14,6 +15,10 @@ public interface NewsScrapRepository extends JpaRepository<NewsScrap, Integer> {
     @Query(value = "SELECT ns FROM NewsScrap ns LEFT JOIN FETCH ns.news n WHERE ns.storageId = :storageId",
            countQuery = "SELECT count(ns) FROM NewsScrap ns WHERE ns.storageId = :storageId")
     Page<NewsScrap> findByStorageIdWithNews(@Param("storageId") Integer storageId, Pageable pageable);
+
+    @Query(value = "SELECT ns FROM NewsScrap ns LEFT JOIN FETCH ns.news n WHERE ns.storageId = :storageId AND n.categoryName = :category",
+           countQuery = "SELECT count(ns) FROM NewsScrap ns JOIN ns.news n WHERE ns.storageId = :storageId AND n.categoryName = :category")
+    Page<NewsScrap> findByStorageIdAndNewsCategoryNameWithNews(@Param("storageId") Integer storageId, @Param("category") Category category, Pageable pageable);
 
     Page<NewsScrap> findByStorageId(Integer storageId, Pageable pageable);
 
