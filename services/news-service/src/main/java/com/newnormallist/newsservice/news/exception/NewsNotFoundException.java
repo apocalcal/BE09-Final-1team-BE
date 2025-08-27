06eabdbe3a022d0d20c3d396cf5b0c531135138d
parkchangjun <parkchangjun13@gmail.com>
2025-08-27 10:41:10 +0900

    [Feat] : 뉴스 조회 실패 시 예외 처리 클래스 추가

--- services/news-service/src/main/java/com/newnormallist/newsservice/news/exception/NewsNotFoundException.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/news/exception/NewsNotFoundException.java
@@ -1,11 +1,12 @@
 package com.newnormallist.newsservice.news.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NewsNotFoundException extends RuntimeException {
+
     public NewsNotFoundException(String message) {
         super(message);
+    }
+
+    public NewsNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
