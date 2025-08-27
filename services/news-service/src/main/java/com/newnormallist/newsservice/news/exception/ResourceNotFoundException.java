d08ab53d7847a9d24180ad8dfaeb8d60505782e6
parkchangjun <parkchangjun13@gmail.com>
2025-08-27 10:43:14 +0900

    [Feat] :  리소스 조회 실패 시 예외 처리 클래스 추가

--- services/news-service/src/main/java/com/newnormallist/newsservice/news/exception/ResourceNotFoundException.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/news/exception/ResourceNotFoundException.java
@@ -3,10 +3,6 @@ package com.newnormallist.newsservice.news.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * 리소스를 찾을 수 없을 때 발생하는 예외
- * 이 예외가 컨트롤러 계층까지 전달되면, Spring은 자동으로 404 Not Found HTTP 상태 코드를 응답
- */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
