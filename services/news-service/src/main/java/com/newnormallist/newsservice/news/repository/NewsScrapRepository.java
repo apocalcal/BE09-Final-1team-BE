110d540c9c00995d97136385f7359fe328319074
parkchangjun <parkchangjun13@gmail.com>
2025-09-01 11:44:05 +0900

    [Fix] : 뉴스 스크랩, 컬렉션 로직 수정

--- services/news-service/src/main/java/com/newnormallist/newsservice/news/repository/NewsScrapRepository.java
+++ services/news-service/src/main/java/com/newnormallist/newsservice/news/repository/NewsScrapRepository.java
@@ -1,6 +1,5 @@
 package com.newnormallist.newsservice.news.repository;
 
-import com.newnormallist.newsservice.news.entity.Category;
 import com.newnormallist.newsservice.news.entity.NewsScrap;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface NewsScrapRepository extends JpaRepository<NewsScrap, Integer> {
@@ -21,11 +19,6 @@ public interface NewsScrapRepository extends JpaRepository<NewsScrap, Integer> {
 
     @Query(value = "SELECT ns FROM NewsScrap ns LEFT JOIN FETCH ns.news n WHERE ns.storageId = :storageId AND n.categoryName = :category",
            countQuery = "SELECT count(ns) FROM NewsScrap ns JOIN ns.news n WHERE ns.storageId = :storageId AND n.categoryName = :category")
-    Page<NewsScrap> findByStorageIdAndNewsCategoryNameWithNews(@Param("storageId") Integer storageId, @Param("category") Category category, Pageable pageable);
-
-    Page<NewsScrap> findByStorageId(Integer storageId, Pageable pageable);
-
-    Optional<NewsScrap> findTopByOrderByScrapIdDesc();
 
     Optional<NewsScrap> findByStorageIdAndNewsNewsId(Integer storageId, Long newsId);
 
@@ -36,5 +29,7 @@ public interface NewsScrapRepository extends JpaRepository<NewsScrap, Integer> {
     @Transactional
     void deleteByStorageId(Integer storageId);
 
-    Page<NewsScrap> findByStorageIdIn(List<Integer> storageIds, Pageable pageable);
+    Page<NewsScrap> findByUserId(Long userId, Pageable pageable);
+
+    Optional<NewsScrap> findByUserIdAndNewsNewsId(Long userId, Long newsId);
 }
